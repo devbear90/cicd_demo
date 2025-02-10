5212707b399fc92f280e5a5eb53e6291ef5f230a
Kovács Richárd <kovacs.richard90@gmail.com>
2025-02-10 21:05:39 +0100

    just a test

---
+++ dags/test_dag copy 3.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'catchup': False
+}
+
+with DAG('test_dag4', default_args=default_args, schedule_interval='@daily') as dag:
+    start = DummyOperator(task_id='start')
+
